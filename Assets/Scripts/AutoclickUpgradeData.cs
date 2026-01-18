fef2744ee359991482c2ec1d3d0b8adc075c3f3f
eggeatingruby2916 <kdj24838@gmail.com>
2026-01-18 20:20:43 +0900

    UIManager 기본적인 완성

--- Assets/Scripts/AutoclickUpgradeData.cs
+++ Assets/Scripts/AutoclickUpgradeData.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "AutoClickUpgradeData", menuName = "ScriptableObject/AutoClickUpgradeData")]
-public class AutoclickUpgradeData : UpgradeData
+public class AutoClickUpgradeData : UpgradeData
 {
     [Header("# Auto Click Upgrade Data")]
     public int addAutoClick;
--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,8 +1,18 @@
 using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
 using TMPro;
 
 public class UIManager : MonoBehaviour
 {
+    public List<Button> upgradeButtons = new List<Button>();
+    
+    public List<ClickUpgradeData> clickUpgradeDatas = new List<ClickUpgradeData>();
+    public List<AutoClickUpgradeData> autoClickUpgradeDatas = new List<AutoClickUpgradeData>();
+    
+    public List<TMP_Text> clickUpgradeButtonTexts = new List<TMP_Text>();
+    public List<TMP_Text> autoClickUpgradeButtonTexts = new List<TMP_Text>();
+    
     public TMP_Text currentGoldText;
     public TMP_Text currentClickText;
     public TMP_Text autoClickText;
@@ -12,10 +22,48 @@ public class UIManager : MonoBehaviour
         currentGoldText.text = $"Current Gold : {GameManager.Instance.GetGold()}";
         currentClickText.text = $"Current Click : {GameManager.Instance.GetClick()}";
         autoClickText.text = $"Auto Click : {GameManager.Instance.GetAutoClick()}";
+
+        for (int i = 0; i < clickUpgradeButtonTexts.Count; i++)
+        {
+            clickUpgradeButtonTexts[i].text = $"Level : {clickUpgradeDatas[i].level} \n Add Click : {clickUpgradeDatas[i].addClick} \n Cost : {clickUpgradeDatas[i].cost}";
+        }
+        
+        for (int i = 0; i < autoClickUpgradeButtonTexts.Count; i++)
+        {
+            autoClickUpgradeButtonTexts[i].text = $"Level : {autoClickUpgradeDatas[i].level} \n Add Auto Click : {autoClickUpgradeDatas[i].addAutoClick} \n Cost : {autoClickUpgradeDatas[i].cost}";
+        }
     }
 
-    public void UpgradeButtonClick()
+    public void MainButtonClick()
     {
-        
+        GameManager.Instance.AddGold(GameManager.Instance.GetClick());
+    }
+
+    public void ClickUpgradeButtonClick(int index)
+    {
+        if (GameManager.Instance.GetGold() >= clickUpgradeDatas[index].cost)
+        {
+            GameManager.Instance.AddClick(clickUpgradeDatas[index].addClick);
+            GameManager.Instance.MinusGold(clickUpgradeDatas[index].cost);
+        }
+
+        else
+        {
+            Debug.Log("돈이 부족합니다.");
+        }
+    }
+
+    public void UpgradeButtonClick(int index)
+    {
+        if (GameManager.Instance.GetGold() >= autoClickUpgradeDatas[index].cost)
+        {
+            GameManager.Instance.AddClick(autoClickUpgradeDatas[index].addAutoClick);
+            GameManager.Instance.MinusGold(autoClickUpgradeDatas[index].cost);
+        }
+
+        else
+        {
+            Debug.Log("돈이 부족합니다.");
+        }
     }
 }
